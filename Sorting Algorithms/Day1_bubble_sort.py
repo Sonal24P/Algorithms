8ec522e9ab3dad39f78fb374234fb0d8981d47fb
Sonal Parmar <70375799+Sonal24P@users.noreply.github.com>
2024-04-30 05:15:59 +0530

    Delete Sorting Algorithms directory

--- Sorting Algorithms/Day1_bubble_sort.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def sortColors(self, nums: List[int]) -> None:
-        for index in range(len(nums)-1):  # take each element from the list
-            for limit in range(index,len(nums)): # take the subsequent element from the list
-                if nums[index]>nums[limit]:  # alter the position when the current element is large
-                    nums[index],nums[limit] =nums[limit],nums[index]    
-        return nums
-        
